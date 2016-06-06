d1311e0b5ad6e33686ce1644e763340f38ad10b7
azhagusurya-m <azhagusuryaa@gmail.com>
2016-06-06 12:22:00 +0530

    Create greatest-3.c

---
+++ greatest-3.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(void) {
+int num1,num2,num3;
+scanf("%d %d %d",&num1,&num2,&num3);
+     printf("%d is the greatest numbers among  %d %d %d ",(num1>num2)?((num1>num3)?num1:num3):(num2>num3)?num2:num3,num1,num2,num3);
+	return 0;
+}
