88880ab39c7dfc8f3079605258067f64da3aebd0
azhagusurya-m <azhagusuryaa@gmail.com>
2016-06-06 21:23:14 +0530

    Create prime_bw_num.c

---
+++ prime_bw_num.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(void) {
+	int num1,num2;
+	int i,flag=0,j;
+	scanf("%d %d",&num1,&num2);
+	for(j=num1;i<num2;j++)
+	{
+	for(i=2;i<=j/2;i++)
+	{
+		if(n%j==0)
+		{
+			 flag=1;
+			 break;
+		}
+	if(flag==1)
+	   	printf("%d\t",j);
+	   	}
+		return 0;
+}
