c53a8ba5f7fb43a533d3241c8d26d52b19788d72
azhagusurya-m <azhagusuryaa@gmail.com>
2016-06-06 11:41:58 +0530

    Create program1.c

---
+++ program1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	int input;
+	 scanf("%d",&input);
+	 if(input>0)
+	  printf("entered input is positive!!!");
+	  else if(input>0)
+	      printf("entered input is negative!!!");
+	      else
+	         printf("entered input is zero!!!");
+	 
+	
+	return 0;
+}
