f0356f801bfcf150056042adcc85fde22a07bf78
azhagusurya-m <azhagusuryaa@gmail.com>
2016-06-06 14:08:43 +0530

    Update naturalsum.c

--- naturalsum.c
+++ naturalsum.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
+
 int main(void) {
 float num,sum=0;
 int n,i;
+printf("enter the number of natural numbers to be added\n");
 scanf("%d",&n);
+printf("enter the natural numbers\n");
   for(i=0;i<n;i++)
   {
   	scanf("%f",&num);
   	sum+=num;
   }
- printf("%f",sum);
-	 return 0;
+  
+printf(" sum:%f",sum);
+	return 0;
 }
