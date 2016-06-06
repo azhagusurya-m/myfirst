f06b651dd299e34d836cc401e49901e59f0dd7da
azhagusurya-m <azhagusuryaa@gmail.com>
2016-06-06 21:16:35 +0530

    Create bw_num.c

---
+++ bw_num.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+	int num1,num2;
+	int i;
+	scanf("%d %d",&num1,&num2);
+	   for(i=num1+1;i<num2;i++)
+	   		if(i%2!=0)
+		      printf("%d\t",i);
+		return 0;
+}
