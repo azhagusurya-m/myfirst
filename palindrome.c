2da00f219d723529a820d3e4552572b87332c848
azhagusurya-m <azhagusuryaa@gmail.com>
2016-06-06 16:17:14 +0530

    Create palindrome.c

---
+++ palindrome.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+int main(void) {
+    int num,i=0,n,p;
+    scanf("%d",&num);
+    int cnum=num;
+    int rev=0	`,mod;
+    while(num)
+    {
+    mod=num%10;
+    num=num/10;
+      rev=rev*10+mod;
+    }
+    
+     if(cnum==rev)
+      printf("palindrome");
+      else
+       printf("not a palindrome");
+       return 0;
+}
