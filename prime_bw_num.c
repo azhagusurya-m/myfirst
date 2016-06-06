2e5385db6422e8dc67e07757868d6c875967325b
azhagusurya-m <azhagusuryaa@gmail.com>
2016-06-06 21:25:44 +0530

    Update prime_bw_num.c

--- prime_bw_num.c
+++ prime_bw_num.c
@@ -3,17 +3,20 @@ int main(void) {
 	int num1,num2;
 	int i,flag=0,j;
 	scanf("%d %d",&num1,&num2);
-	for(j=num1;i<num2;j++)
+	for(j=num1;j<num2;j++)
 	{
 	for(i=2;i<=j/2;i++)
 	{
-		if(n%j==0)
+		if(j%i==0)
 		{
 			 flag=1;
 			 break;
 		}
-	if(flag==1)
+	}
+	if(flag!=1)
 	   	printf("%d\t",j);
-	   	}
+	   	flag=0;
+	   	
+	}
 		return 0;
 }
