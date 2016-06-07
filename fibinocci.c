b6f08be5911257c53dd1d92fcd238224e10463f6
azhagusurya-m <azhagusuryaa@gmail.com>
2016-06-07 14:18:35 +0530

    Create fibinocci.c

---
+++ fibinocci.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ int main(void) {
+  int sum=0,n,i;
+   printf("enter the number\n");
+    scanf("%d",&n);
+    printf("1\t");
+     for(i=1;i<=n;i++)
+     {
+     	
+     	sum+=i;
+     	printf("%d\t",sum);
+     }
+	return 0;
+}
