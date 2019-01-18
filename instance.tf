883f03db7974f5259fa634b6348d879437b2e790
azhagusurya-m <azhagusuryaa@gmail.com>
2019-01-18 18:19:30 +0530

    Create instance.tf

---
+++ instance.tf
@@ -0,0 +1,47 @@
+## Approved "testing" AMIs
+
+ default = {
+
+    coreos                 = "ami-6db03312" # 2018-05 - This AMI gets regenerated on an on-demand basis
+
+    windows-2008r2-gravity = "ami-0b9f8035fc74ad13a" # ACOM-Win2K8R2-GRAVITY (2018-09)
+
+    windows-2012-sql-base  = "ami-24f1a15e" # Base image
+
+    windows-2012-sql       = "ami-d7cdf5ad" # Post-processed by SQL team
+
+ 
+
+    amazon-linux-base          = "ami-0d4acfe0866d14cc3" # 2018-12
+
+    amazon-linux2-base         = "ami-00e60799bf4c9dd6e" # 2018-12
+
+    amazon-linux-deep-learning = "ami-0c97fda6427ad072c" # 2018-12
+
+    amazon-linux-ecs           = "ami-0ba1751d9d5e67586" # 2018-12
+
+    amazon-linux-jenkins       = "ami-0c755c42219d8c075" # 2018-12
+
+    jumphost                   = "ami-02d08190e43cf1b67" # 2018-12
+
+    centos7-base               = "ami-0a9d9b03b71195ccd" # 2018-12
+
+    centos6-base               = "ami-074087a94497fc491" # ACOM-CentOS6-Base-2018-10
+
+   
+
+    
+
+    windows-2008r2-base = "ami-01c31953af0be46cb"       # 2018-12
+
+    windows-2012r2-base = "ami-01774108fcd005586"       # 2018-12
+
+    windows-2016-iis = "ami-038e99c423c6b1b89"              # 2018-09
+
+ 
+
+    #ubuntu-14-04-LTS-base      = "ami-4571c63f" # Retired
+
+  }
+
+}
